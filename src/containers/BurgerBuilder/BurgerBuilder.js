51ac23218952cd125055900702a090ed90e856b4
Manas-saxena <manassaxena160601@gmail.com>
2020-09-15 16:57:50 +0530

    controls added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,24 +1,22 @@
 import React ,{Component} from 'react';
 import Aux from '../../hoc/Auxi';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 class BurgerBuilder extends Component{
-    state={
+    state = {
         ingredients:{
-            salad:1,
-            bacon:2,
-            cheese:2,
-            meat:2
+            salad:0,
+            bacon:0,
+            cheese:0,
+            meat:0
         }
     }
+    
     render(){
             return (
                 <Aux>
-                    <div>
-                        <Burger ingredients={this.sate.ingredients}  />
-                    </div>
-                    <div>
-                        Build control
-                    </div>
+                    <Burger ingredients={this.state.ingredients} />
+                    <BuildControls />
                 </Aux>
             );
     }
